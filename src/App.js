f6ad4d9b9658bdbc3803ed786b0ed71194b77ada
ToniJukkola <n1juto00@students.oamk.fi>
2022-02-01 20:42:34 +0200

    Peruselementit tehty

--- src/App.js
+++ src/App.js
@@ -4,8 +4,27 @@ import './App.css';
 function App() {
   return (
     <div>
-   <h1>Moivaanmoi</h1>
-   <p>Nyt vähän kokeillaan mitä tapahtuu.</p>
+   <h1>Calculating alcohol blood level</h1>
+   <br></br>
+   <p>Weight
+     <input type="number">
+     </input>
+     </p>
+     <p>Bottles
+       <input type="number">
+     </input>
+     </p>
+     <p>Time
+       <input type="number">
+     </input>
+     </p>
+     <p>Gender
+       <input type="radio">
+     </input>Male
+     <input type="radio">
+     </input>Female</p>
+     <br></br>
+     <button>Calculate</button>
    </div>
   );
 }
